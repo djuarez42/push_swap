a2bb2d555568d497f2b8541c062372205258ecdb
Daniel Juarez <djuarez@car9s2.42barcelona.com>
2025-01-23 10:28:50 +0100

    updated

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42barcelona.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 07:52:52 by djuarez           #+#    #+#             */
-/*   Updated: 2025/01/20 08:52:13 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/01/23 09:52:29 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,38 @@
 #include "ft_printf/ft_printf.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
+
+typedef struct	s_stack_node
+{
+	int					value;
+	int					current_position;
+	int					final_index;
+	int					push_price;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}				t_stack_node;
+
+//*** Handle errors/free ***
+
+void		free_matrix(char **argv);
+void		free_stack(t_stack_node **stack);
+void		error_free(t_stack_node **a, char **argv, bool flag_argc_2);
+int		error_syntax(char *str_nbr);
+int		error_repetition(t_stack_node *a, int nbr);
+
+//*** linked list utils ***
+t_stack_node    *find_last_node(t_stack_node *head);
+void    append_node(t_stack_node **stack, int nbr);
+t_stack_node    *find_smallest(t_stack_node *stack);
+t_stack_node    *return_cheapest(t_stack_node *stack);
+int     stack_len(t_stack_node *stack);
+
+
+
+
 
 #endif
---
+++ stack_init.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djuarez <djuarez@student.42barcelona.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 08:42:37 by djuarez           #+#    #+#             */
+/*   Updated: 2025/01/23 10:13:19 by djuarez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static long	ft_atol(const char *str)
+{
+	long	num;
+	int		isneg;
+	int		i;
+
+	num = 0;
+	isneg = 1;
+	i = 0;
+	while (str[i] && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + str[i] - '0';
+		i++;
+	}
+	return (num * isneg);
+}
+
+void	stack_init(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	long	nbr;
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
+			error_free(a, argv, flag_argc_2);
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error_free(a, argv, flag_argc_2);
+		if (error_repetition(*a, (int)nbr))
+			error_free(a, argv, flag_argc_2);
+		append_node(a, (int)nbr);
+		i++;
+	}
+	if (flag_argc_2)
+		free_matrix(argv);
+}
